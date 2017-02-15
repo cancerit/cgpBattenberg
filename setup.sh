bb781355989943c205ff1f74dcb2c94e23d4a8b0
Keiran Raine <kr2@sanger.ac.uk>
2017-02-15 12:41:58 +0000

    Clean up prereq and build

--- setup.sh
+++ setup.sh
@@ -21,19 +21,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ########## LICENCE ##########
 
-done_message () {
-    if [ $? -eq 0 ]; then
-        echo " done."
-        if [ "x$1" != "x" ]; then
-            echo $1
-        fi
-    else
-        echo " failed.  See setup.log file for error messages." $2
-        echo "    Please check INSTALL file for items that should be installed by a package manager"
-        exit 1
-    fi
-}
-
 if [[ ($# -ne 1 && $# -ne 2) ]] ; then
   echo "Please provide an installation path such as /opt/pancan and optionally perl lib paths to allow, e.g."
   echo "  ./setup.sh /opt/myBundle"
@@ -69,21 +56,32 @@ fi
 SETUP_DIR=$INIT_DIR/install_tmp
 mkdir -p $SETUP_DIR
 
-# re-initialise log file
-echo > $INIT_DIR/setup.log
-
 # log information about this system
-(
-    echo '============== System information ===='
-    set -x
-    lsb_release -a
-    uname -a
-    sw_vers
-    system_profiler
-    grep MemTotal /proc/meminfo
-    set +x
-    echo; echo
-) >>$INIT_DIR/setup.log 2>&1
+echo '============== System information ===='
+set -x
+lsb_release -a
+uname -a
+sw_vers
+system_profiler
+grep MemTotal /proc/meminfo
+set +x
+echo; echo
+
+## cpanm will have been installed by the pre-requisites:
+CPANM=`which cpanm`
+echo $CPANM
+
+perlmods=( "version" "File::ShareDir" "File::ShareDir::Install" )
+
+set -e
+for i in "${perlmods[@]}" ; do
+  echo -n "Installing build prerequisite $i..."
+  set -x
+  $CPANM -v --mirror http://cpan.metacpan.org -l $INST_PATH $i
+  set +x
+  echo; echo
+done
+set +e
 
 PCAP=`perl -le 'eval "require $ARGV[0]" and print $ARGV[0]->VERSION' PCAP`
 if [[ "x$PCAP" == "x" ]] ; then
@@ -93,8 +91,8 @@ if [[ "x$PCAP" == "x" ]] ; then
 else
   # need the leading 'v' on versions so comparison of those that don't have hotfix element behave correctly, i.e. (X.X, rather than X.X.X)
   GOOD_VER=`perl -Mversion -e "version->parse(q{v$PCAP}) >= version->parse(q{v}.q{1.12}) ? print qq{1\n} : print qq{0\n};"`
-  if [[ $GOOD_VER -ne '1' ]]; then
-    echo "PREREQUISITE: Please install PCAP-core (v1.12+) before proceeding:"
+  if [ $GOOD_VER -ne 1 ]; then
+    echo "PREREQUISITE: Please install PCAP-core (v1.12+) before proceeding (version too old):"
     echo "  https://github.com/ICGC-TCGA-PanCancer/PCAP-core/releases"
     exit 1;
   fi
@@ -122,24 +120,7 @@ else
   fi
 fi
 
-## cpanm will have been installed by the pre-requisites:
-CPANM=`which cpanm`
-echo $CPANM
-
-perlmods=( "File::ShareDir" "File::ShareDir::Install" )
-
 set -e
-for i in "${perlmods[@]}" ; do
-  echo -n "Installing build prerequisite $i..."
-  (
-    set -x
-    $CPANM -v --mirror http://cpan.metacpan.org -l $INST_PATH $i
-    set +x
-    echo; echo
-  ) >>$INIT_DIR/setup.log 2>&1
-  done_message "" "Failed during installation of $i."
-done
-
 cd $INIT_DIR/perl
 
 echo -n "Installing Perl prerequisites ..."
@@ -147,21 +128,15 @@ if ! ( perl -MExtUtils::MakeMaker -e 1 >/dev/null 2>&1); then
     echo
     echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar."
 fi
-(
-  set -x
-  $CPANM -v --mirror http://cpan.metacpan.org --notest -l $INST_PATH/ --installdeps . < /dev/null
-  set +x
-) >>$INIT_DIR/setup.log 2>&1
-done_message "" "Failed during installation of core dependencies."
+set -x
+$CPANM -v --mirror http://cpan.metacpan.org --notest -l $INST_PATH/ --installdeps . < /dev/null
+set +x
 
 echo -n "Installing cgpBattenberg ..."
-(
-  perl Makefile.PL INSTALL_BASE=$INST_PATH
-  make
-  make test
-  make install
-) >>$INIT_DIR/setup.log 2>&1
-done_message "" "cgpBattenberg install failed."
+perl Makefile.PL INSTALL_BASE=$INST_PATH
+make
+make test
+make install
 
 # cleanup all junk
 rm -rf $SETUP_DIR
