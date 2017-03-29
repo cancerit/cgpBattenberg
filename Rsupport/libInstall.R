395183bf89b523b784c3c5461e85ffe23cdd3fa9
Kathryn Beal <kb3@sanger.ac.uk>
2017-03-29 16:11:08 +0100

    Added setup for R libraries

---
+++ Rsupport/libInstall.R
@@ -0,0 +1,16 @@
+args<-commandArgs(TRUE)
+instLib = args[1]
+ascatPackage = args[2]
+battenbergPackage = args[3]
+
+r = getOption("repos") # hard code the UK repo for CRAN
+r["CRAN"] = "http://cran.uk.r-project.org"
+options(repos = r)
+rm(r)
+#Use previous version because current version in cpan is not compatible with R < 3.2 (dir.exists).
+install.packages("http://cran.uk.r-project.org/src/contrib/Archive/stringi/stringi_1.1.2.tar.gz", repos=NULL, type="source")
+install.packages("RColorBrewer")
+install.packages("ggplot2")
+install.packages("readr")
+install.packages(ascatPackage)
+install.packages(battenbergPackage)
