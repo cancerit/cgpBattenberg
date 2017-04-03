4cac472284aef592994dbd68f23109c3498f0049
Keiran Raine <kr2@sanger.ac.uk>
2017-04-03 14:53:56 +0100

    Release with verion bump

--- Rsupport/libInstall.R
+++ Rsupport/libInstall.R
@@ -7,7 +7,7 @@ r = getOption("repos") # hard code the UK repo for CRAN
 r["CRAN"] = "http://cran.uk.r-project.org"
 options(repos = r)
 rm(r)
-#Use previous version because current version in cpan is not compatible with R < 3.2 (dir.exists).
+#Use previous version because current version in cran is not compatible with R < 3.2 (dir.exists).
 install.packages("http://cran.uk.r-project.org/src/contrib/Archive/stringi/stringi_1.1.2.tar.gz", repos=NULL, type="source")
 install.packages("RColorBrewer")
 install.packages("ggplot2")
