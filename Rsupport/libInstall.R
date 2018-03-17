1f159e2c7e4e31c34d799fff238e77da25106add
Keiran Raine <kr2@sanger.ac.uk>
2018-03-17 08:58:59 +0000

    Resolves #60, just double check with older R

--- Rsupport/libInstall.R
+++ Rsupport/libInstall.R
@@ -3,14 +3,34 @@ instLib = args[1]
 ascatPackage = args[2]
 battenbergPackage = args[3]
 
+#Use previous version because current version in cran is not compatible with R < 3.2 (dir.exists).
+stringi_for_legacyR = "1.1.2"
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    biocLite(new.pkg, ask=FALSE, lib=instLib)
+  sapply(pkg, library, character.only = TRUE)
+}
+
 r = getOption("repos") # hard code the UK repo for CRAN
 r["CRAN"] = "http://cran.uk.r-project.org"
 options(repos = r)
 rm(r)
-#Use previous version because current version in cran is not compatible with R < 3.2 (dir.exists).
-install.packages("http://cran.uk.r-project.org/src/contrib/Archive/stringi/stringi_1.1.2.tar.gz", repos=NULL, type="source")
-install.packages("RColorBrewer")
-install.packages("ggplot2")
-install.packages("readr")
-install.packages(ascatPackage)
-install.packages(battenbergPackage)
+source("http://bioconductor.org/biocLite.R")
+
+tmp <- c("devtools")
+ipak(tmp)
+library(devtools)
+options(download.file.method = "auto")
+
+if ( version$major >= 3 && version$minor >= 2 ) {
+  biocPackages <- c("stringi", "readr", "doParallel", "ggplot2", "RColorBrewer", "gridExtra", "gtools")
+} else {
+  install_version("stringi", stringi_for_legacyR)
+  biocPackages <- c("readr", "doParallel", "ggplot2", "RColorBrewer", "gridExtra", "gtools")
+}
+
+ipak(biocPackages)
+install_file(ascatPackage)
+install_file(battenbergPackage)
