de7d8b96b5e0e5976b0d7d5ea5fac78a796a49bd
Raul Alcantara <ra11@sanger.ac.uk>
2019-09-18 15:54:02 +0000

    initial working version of container code

---
+++ Dockerfile
@@ -0,0 +1,94 @@
+FROM  quay.io/wtsicgp/dockstore-cgpmap:3.1.4 as builder
+
+USER  root
+
+# ALL tool versions used by opt-build.sh
+ENV VER_ALLELECOUNT="4.1.0"
+ENV VER_CGPVCF="v2.2.1"
+
+RUN apt-get -yq update
+RUN apt-get install -yq --no-install-recommends \
+locales \
+g++ \
+make \
+gcc \
+pkg-config \
+python \
+zlib1g-dev \
+r-base \
+libbz2-dev \
+liblzma-dev \
+libcurl4-openssl-dev \
+libssl-dev \
+libxml2-dev \
+libssh2-1-dev
+
+RUN locale-gen en_US.UTF-8
+RUN update-locale LANG=en_US.UTF-8
+
+ENV OPT /opt/wtsi-cgp
+ENV PATH $OPT/bin:$OPT/biobambam2/bin:$PATH
+ENV PERL5LIB $OPT/lib/perl5
+ENV LD_LIBRARY_PATH $OPT/lib
+ENV LC_ALL en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV R_LIBS $OPT/R-lib
+ENV R_LIBS_USER $R_LIBS
+
+# build tools from other repos
+ADD build/opt-build.sh build/
+RUN bash build/opt-build.sh $OPT
+
+# build the tools in this repo, separate to reduce build time on errors
+COPY . .
+RUN bash build/opt-build-local.sh $OPT
+
+FROM ubuntu:16.04
+
+LABEL maintainer="cgphelp@sanger.ac.uk" \
+      uk.ac.sanger.cgp="Cancer, Ageing and Somatic Mutation, Wellcome Trust Sanger Institute" \
+      version="v1.0.0" \
+      description="cgpCaVEManWrapper docker"
+
+RUN apt-get -yq update
+RUN apt-get install -yq --no-install-recommends \
+apt-transport-https \
+locales \
+curl \
+ca-certificates \
+libperlio-gzip-perl \
+bzip2 \
+psmisc \
+time \
+zlib1g \
+liblzma5 \
+libncurses5 \
+p11-kit \
+r-base \
+unattended-upgrades && \
+unattended-upgrade -d -v && \
+apt-get remove -yq unattended-upgrades && \
+apt-get autoremove -yq
+
+RUN locale-gen en_US.UTF-8
+RUN update-locale LANG=en_US.UTF-8
+
+ENV OPT /opt/wtsi-cgp
+ENV PATH $OPT/bin:$OPT/biobambam2/bin:$PATH
+ENV PERL5LIB $OPT/lib/perl5
+ENV LD_LIBRARY_PATH $OPT/lib
+ENV LC_ALL en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV R_LIBS $OPT/R-lib
+ENV R_LIBS_USER $R_LIBS
+
+RUN mkdir -p $OPT
+COPY --from=builder $OPT $OPT
+
+## USER CONFIGURATION
+RUN adduser --disabled-password --gecos '' ubuntu && chsh -s /bin/bash && mkdir -p /home/ubuntu
+
+USER    ubuntu
+WORKDIR /home/ubuntu
+
+CMD ["/bin/bash"]
---
+++ build/opt-build.sh
@@ -0,0 +1,102 @@
+#! /bin/bash
+
+set -xe
+
+if [[ -z "${TMPDIR}" ]]; then
+  TMPDIR=/tmp
+fi
+
+set -u
+
+if [ "$#" -lt "1" ] ; then
+  echo "Please provide an installation path such as /opt/ICGC"
+  exit 1
+fi
+
+# get path to this script
+SCRIPT_PATH=`dirname $0`;
+SCRIPT_PATH=`(cd $SCRIPT_PATH && pwd)`
+
+# get the location to install to
+INST_PATH=$1
+mkdir -p $1
+INST_PATH=`(cd $1 && pwd)`
+echo $INST_PATH
+
+# get current directory
+INIT_DIR=`pwd`
+
+CPU=`grep -c ^processor /proc/cpuinfo`
+if [ $? -eq 0 ]; then
+  if [ "$CPU" -gt "6" ]; then
+    CPU=6
+  fi
+else
+  CPU=1
+fi
+echo "Max compilation CPUs set to $CPU"
+
+SETUP_DIR=$INIT_DIR/install_tmp
+mkdir -p $SETUP_DIR/distro # don't delete the actual distro directory until the very end
+mkdir -p $INST_PATH/bin
+cd $SETUP_DIR
+
+# make sure tools installed can see the install loc of libraries
+set +u
+export LD_LIBRARY_PATH=`echo $INST_PATH/lib:$LD_LIBRARY_PATH | perl -pe 's/:\$//;'`
+export PATH=`echo $INST_PATH/bin:$PATH | perl -pe 's/:\$//;'`
+export MANPATH=`echo $INST_PATH/man:$INST_PATH/share/man:$MANPATH | perl -pe 's/:\$//;'`
+export PERL5LIB=`echo $INST_PATH/lib/perl5:$PERL5LIB | perl -pe 's/:\$//;'`
+set -u
+
+export VER_VCFTOOLS='0.1.16'
+## vcftools
+if [ ! -e $SETUP_DIR/vcftools.success ]; then
+  curl -sSL --retry 10 https://github.com/vcftools/vcftools/releases/download/v${VER_VCFTOOLS}/vcftools-${VER_VCFTOOLS}.tar.gz > distro.tar.gz
+  rm -rf distro/*
+  tar --strip-components 2 -C distro -xzf distro.tar.gz
+  cd distro
+  ./configure --prefix=$INST_PATH --with-pmdir=lib/perl5
+  make -j$CPU
+  make install
+  cd $SETUP_DIR
+  rm -rf distro.* distro/*
+  touch $SETUP_DIR/vcftools.success
+fi
+
+### cgpVcf
+if [ ! -e $SETUP_DIR/cgpVcf.success ]; then
+  curl -sSL --retry 10 https://github.com/cancerit/cgpVcf/archive/${VER_CGPVCF}.tar.gz > distro.tar.gz
+  rm -rf distro/*
+  tar --strip-components 1 -C distro -xzf distro.tar.gz
+  cd distro
+  cpanm --no-interactive --notest --mirror http://cpan.metacpan.org --notest -l $INST_PATH --installdeps .
+  cpanm -v --no-interactive --mirror http://cpan.metacpan.org -l $INST_PATH .
+  cd $SETUP_DIR
+  rm -rf distro.* distro/*
+  touch $SETUP_DIR/cgpVcf.success
+fi
+
+### alleleCount
+echo "Building alleleCounter ..."
+if [ ! -e $SETUP_DIR/alleleCount.success ]; then
+  curl -sSL --retry 10 https://github.com/cancerit/alleleCount/archive/${VER_ALLELECOUNT}.tar.gz > distro.tar.gz
+  rm -rf distro/*
+  tar --strip-components 1 -C distro -xzf distro.tar.gz
+  #build the C part
+  cd distro
+  make -C c clean
+  export prefix=$INST_PATH
+  make -C c -j$CPU
+  cp $SETUP_DIR/distro/c/bin/alleleCounter $INST_PATH/bin/.
+  #build the perl part
+  cd $SETUP_DIR/distro/perl  
+  cpanm --no-interactive --notest --mirror http://cpan.metacpan.org -l $INST_PATH/ --installdeps $SETUP_DIR/distro/perl/. < /dev/null
+  perl Makefile.PL INSTALL_BASE=$INST_PATH
+  make
+  make test
+  make install
+  cd $SETUP_DIR
+  rm -rf distro.* distro/*  
+  touch $SETUP_DIR/alleleCounter.success
+fi
